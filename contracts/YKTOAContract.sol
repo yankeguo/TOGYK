b11f7eed602b472b8e0e48c33a4f863a81663b25
GUO YANKE <hi@yankeguo.com>
2024-07-09 19:14:17 +0800

    refactor: simplify contract

--- contracts/YKTOAContract.sol
+++ contracts/YKTOAContract.sol
@@ -4,13 +4,12 @@ pragma solidity ^0.8.20;
 
 import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import {IERC1155MetadataURI} from "@openzeppelin/contracts/token/ERC1155/extensions/IERC1155MetadataURI.sol";
-import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
-import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import {IERC165, ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
-import {AccessControlDefaultAdminRules} from "@openzeppelin/contracts/access/extensions/AccessControlDefaultAdminRules.sol";
 import {IAccessControlDefaultAdminRules} from "@openzeppelin/contracts/access/extensions/IAccessControlDefaultAdminRules.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import {AccessControlDefaultAdminRules} from "@openzeppelin/contracts/access/extensions/AccessControlDefaultAdminRules.sol";
 
-contract YKTOAContract is ERC1155, AccessControlDefaultAdminRules {
+contract YKTOAContract is ERC1155Supply, AccessControlDefaultAdminRules {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor()
@@ -19,6 +18,10 @@ contract YKTOAContract is ERC1155, AccessControlDefaultAdminRules {
         )
         AccessControlDefaultAdminRules(3 days, msg.sender)
     {}
+
+    function setURI(string memory newURI) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _setURI(newURI);
+    }
 
     function supportsInterface(
         bytes4 interfaceId
@@ -34,132 +37,5 @@ contract YKTOAContract is ERC1155, AccessControlDefaultAdminRules {
             interfaceId == type(IERC1155MetadataURI).interfaceId ||
             interfaceId == type(IAccessControlDefaultAdminRules).interfaceId ||
             super.supportsInterface(interfaceId);
-    }
-
-    /************************************
-     * ERC1155URIStorage
-     ************************************/
-
-    using Strings for uint256;
-
-    // Optional base URI
-    string private _baseURI = "";
-
-    // Optional mapping for token URIs
-    mapping(uint256 tokenId => string) private _tokenURIs;
-
-    /**
-     * @dev See {IERC1155MetadataURI-uri}.
-     *
-     * This implementation returns the concatenation of the `_baseURI`
-     * and the token-specific uri if the latter is set
-     *
-     * This enables the following behaviors:
-     *
-     * - if `_tokenURIs[tokenId]` is set, then the result is the concatenation
-     *   of `_baseURI` and `_tokenURIs[tokenId]` (keep in mind that `_baseURI`
-     *   is empty per default);
-     *
-     * - if `_tokenURIs[tokenId]` is NOT set then we fallback to `super.uri()`
-     *   which in most cases will contain `ERC1155._uri`;
-     *
-     * - if `_tokenURIs[tokenId]` is NOT set, and if the parents do not have a
-     *   uri value set, then the result is empty.
-     */
-    function uri(
-        uint256 tokenId
-    ) public view virtual override returns (string memory) {
-        string memory tokenURI = _tokenURIs[tokenId];
-
-        // If token URI is set, concatenate base URI and tokenURI (via string.concat).
-        return
-            bytes(tokenURI).length > 0
-                ? string.concat(_baseURI, tokenURI)
-                : super.uri(tokenId);
-    }
-
-    /**
-     * @dev Sets `tokenURI` as the tokenURI of `tokenId`.
-     */
-    function _setURI(uint256 tokenId, string memory tokenURI) internal virtual {
-        _tokenURIs[tokenId] = tokenURI;
-        emit URI(uri(tokenId), tokenId);
-    }
-
-    /**
-     * @dev Sets `baseURI` as the `_baseURI` for all tokens
-     */
-    function _setBaseURI(string memory baseURI) internal virtual {
-        _baseURI = baseURI;
-    }
-
-    /************************************
-     * ERC1155Supply
-     ************************************/
-
-    mapping(uint256 id => uint256) private _totalSupply;
-    uint256 private _totalSupplyAll;
-
-    /**
-     * @dev Total value of tokens in with a given id.
-     */
-    function totalSupply(uint256 id) public view virtual returns (uint256) {
-        return _totalSupply[id];
-    }
-
-    /**
-     * @dev Total value of tokens.
-     */
-    function totalSupply() public view virtual returns (uint256) {
-        return _totalSupplyAll;
-    }
-
-    /**
-     * @dev Indicates whether any token exist with a given id, or not.
-     */
-    function exists(uint256 id) public view virtual returns (bool) {
-        return totalSupply(id) > 0;
-    }
-
-    /**
-     * @dev See {ERC1155-_update}.
-     */
-    function _update(
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory values
-    ) internal virtual override {
-        super._update(from, to, ids, values);
-
-        if (from == address(0)) {
-            uint256 totalMintValue = 0;
-            for (uint256 i = 0; i < ids.length; ++i) {
-                uint256 value = values[i];
-                // Overflow check required: The rest of the code assumes that totalSupply never overflows
-                _totalSupply[ids[i]] += value;
-                totalMintValue += value;
-            }
-            // Overflow check required: The rest of the code assumes that totalSupplyAll never overflows
-            _totalSupplyAll += totalMintValue;
-        }
-
-        if (to == address(0)) {
-            uint256 totalBurnValue = 0;
-            for (uint256 i = 0; i < ids.length; ++i) {
-                uint256 value = values[i];
-
-                unchecked {
-                    // Overflow not possible: values[i] <= balanceOf(from, ids[i]) <= totalSupply(ids[i])
-                    _totalSupply[ids[i]] -= value;
-                    // Overflow not possible: sum_i(values[i]) <= sum_i(totalSupply(ids[i])) <= totalSupplyAll
-                    totalBurnValue += value;
-                }
-            }
-            unchecked {
-                // Overflow not possible: totalBurnValue = sum_i(values[i]) <= sum_i(totalSupply(ids[i])) <= totalSupplyAll
-                _totalSupplyAll -= totalBurnValue;
-            }
-        }
     }
 }
